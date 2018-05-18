0575382173d2042b858e316691ef0b111e8284f5
Kasinathan <BHARATH_KASINATHAN1@homedepot.com>
2018-05-18 15:11:16 +0530

    Developer Profile capture

--- extension.js
+++ extension.js
@@ -12,21 +12,47 @@ function activate(context) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension is install');
 
+    context.subscriptions.push(vscode.commands.registerCommand('captureDevInformation.start', () => {
+        // Create and show panel
+        var panel = vscode.window.createWebviewPanel('catCoding', "Developer Profile", vscode.ViewColumn.One, { });
+
+        // And set its HTML content
+        panel.webview.html = getWebviewContent();
+
+
+        panel.webview.onDidReceiveMessage(message => {
+            switch (message.command) {
+                case 'alert':
+                    vscode.window.showErrorMessage(message.text);
+                    return;
+            }
+        }, undefined, context.subscriptions);
+
+        // Reset when the current panel is closed
+        panel.onDidDispose(() => {
+            panel = undefined;
+        }, null, context.subscriptions);
+    }));
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
 
+    // vscode.workspace.openTextDocument('./profile.json').then((document) => {
+    //     let text = document.getText();
+    //     console.log(text);
+    //   }).fail((err)=>{
+    //       console.log(err);
+    //   });
+
+
     vscode.commands.registerCommand('extension.hello', () => {
         console.log('hello extension is executed');
-        let editor = vscode.window.activeTextEditor;
 
-        // let selection = editor.selection;
-        let text = editor.document.getText();
-
-        if (text.indexOf('test') >= 0) {
             vscode.window.showInformationMessage('Hello World!');
-        }
+
     });
+
 
     let disposable = vscode.commands.registerCommand('extension.activateBot', function () {
         // The code you place here will be executed every time your command is executed
@@ -59,6 +85,10 @@ function activate(context) {
 
     });
 
+    
+    // vscode.commands.executeCommand('extension.hello');
+
+    
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
@@ -67,3 +97,36 @@ exports.activate = activate;
 function deactivate() {
 }
 exports.deactivate = deactivate;
+
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+    <html>
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src vscode-resource:; style-src vscode-resource:;">
+    <body>
+    
+    <h1>Hi there! I am intqu bot.</h1>
+    
+    <h2>Say something about you...</h2>
+    <form>
+     Name: <input type="text" id="devName" name="name"> <br> <br>
+     Experience (in years): <input type="number" name="experience"><br> <br>
+     Technology(comma seperated): <input type="text" name="technology"><br> <br>
+    </form>
+    
+    <br> <br>
+    <input type="checkbox" name="design" value="Responsive">Is your design Responsive<br> <br>
+    <input type="checkbox" name="design" value="Responsive">Are you going to teach me?<br>
+    <br>
+    <button onclick="saveData()">Ok</button>
+    </body>
+    <script>
+    function saveData() {
+        var developerName  = document.getElementById("devName");
+        vscode.postMessage({
+            command: 'alert',
+            text: '🐛  on line ' + developerName
+        })
+      }
+    </script>
+    </html>`;
+}
