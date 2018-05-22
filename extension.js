8d0a865ddd85c9315547bca4d690a57daf37a9c8
Kasinathan <BHARATH_KASINATHAN1@homedepot.com>
2018-05-22 14:29:58 +0530

    show information on save developer profile

--- extension.js
+++ extension.js
@@ -49,7 +49,7 @@ function activate(context) {
     vscode.commands.registerCommand('extension.hello', () => {
         console.log('hello extension is executed');
 
-            vscode.window.showInformationMessage('Hello World!');
+            vscode.window.showInformationMessage('Hello World !!');
 
     });
 
@@ -104,68 +104,46 @@ function deactivate() {
 exports.deactivate = deactivate;
 
 function getWebviewContent() {
-    return `<!DOCTYPE html>
-    <html>
-    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:;script-src https: 'unsafe-inline' vscode-resource:; style-src vscode-resource:;">
-    <script>
-    function saveData(developerName) {
+    return `
+    <!DOCTYPE html>
+<html>
+   <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:;script-src https: 'unsafe-inline' vscode-resource:; style-src vscode-resource:;">
+   <script>
+      function saveData(developerName) {
 
-
-        var x = document.getElementById("showInformation");
+          var x = document.getElementById("showInformation");
           x.innerText = "Hello " + developerName + ", we saved your information!!";
-
-        // const vscode = acquireVsCodeApi();
-        // var developerName  = document.getElementById("devName");
-        //vscode.postMessage({command: 'alert',text: '🐛  on line ' + developerName})
-        //window.parent.postMessage({type: 'dev-details',devName: developerName});
-
-
-      }
-    </script>
-    <style>
-.button {
-    background-color: #4CAF50;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
-}
-</style>
-    <body>
-    
-    <h1>Hi there! I am intqu bot.</h1>
-    
-    <h2>Say something about you...</h2>
-    <form>
-     Name: <input type="text" id="devName" name="name"> <br> <br>
-     Experience (in years): <input type="number" id="devexp" name="experience"><br> <br>
-     Technology(comma seperated): <input type="text" id="devTech" name="technology"><br> <br>
-    </form>
-    
-    <br>
-    
-    <input type="checkbox" name="resposive" value="resposive">Responsive
-<br>
-<input type="checkbox" name="lingual" value="lingual">Multilingual
-    
-    
-    <br>
-    <h3>
-    <input type="radio" name="mode" value="learn">Learning mode
-    </h3>
-        <h3>
-    <input type="radio" name="mode" value="expert">Expert mode
-  </h3>
-    <button type="button" class="button" onclick="saveData(devName.value,devexp.value,devTech.value)">Ok</button>
-    <br>
-    <h2 id="showInformation">
-    </h2>
-    </body>
-    
-    </html>`;
+      
+          // const vscode = acquireVsCodeApi();
+          // var developerName  = document.getElementById("devName");
+          //vscode.postMessage({command: 'alert',text: '🐛  on line ' + developerName})
+          //window.parent.postMessage({type: 'dev-details',devName: developerName});
+        }
+   </script>
+   <body>
+      <h1>Hi there! I am IntQu bot.</h1>
+      <h2>Say something about you...</h2>
+      <form>
+         Name: <input type="text" id="devName" name="name"> <br> <br>
+         Experience (in years): <input type="number" id="devexp" name="experience"><br> <br>
+         Technology(comma seperated): <input type="text" id="devTech" name="technology"><br> <br>
+      </form>
+      <br>
+      <input type="checkbox" name="resposive" value="resposive">Responsive
+      <input type="checkbox" name="lingual" value="lingual">Multilingual
+      <br>
+      <br>
+      <h3>
+         <input type="radio" name="mode" value="learn">Learning mode
+      </h3>
+      <h3>
+         <input type="radio" name="mode" value="expert">Expert mode
+      </h3>
+      <input style="background-color:#4CAF50;font-size: 24px;" value="Save" type="button"  onclick="saveData(devName.value,devexp.value,devTech.value)">
+      <br>
+      <h2 id="showInformation" style="color:red;">
+      </h2>
+   </body>
+</html>
+`;
 }
